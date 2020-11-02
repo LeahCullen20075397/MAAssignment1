6a5e07df833f916c40e7ccdb996c64a02f1bb83e
Leah Cullen <leahclcv@gmail.com>
2020-11-02 14:41:49 +0000

    setup main menu

--- src/main/kotlin/ie/wit/LeahCullen/A1/main.kt
+++ src/main/kotlin/ie/wit/LeahCullen/A1/main.kt
@@ -7,4 +7,38 @@ private val logger = KotlinLogging.logger{}
 fun main(args: Array<String>){
     logger.info{"Launching Dragon Age Console App"}
     println("Dragon Age Console App")
+
+    var input: Int
+
+    do{
+        input = mainMenu()
+        when(input){
+            1 -> println("You chose Create New Playthrough")
+            2 -> println("You chose Update A Playthrough")
+            3 -> println("You chose List All Playthroughs")
+            0 -> println("Exiting app...")
+            else -> println("Invalid option. Please try again")
+        }
+        println()
+    }while(input != 0)
+    logger.info{"Shutting Down Dragon Age Console App"}
+}
+
+fun mainMenu(): Int{
+    var option: Int
+    var input: String? = null
+
+    println("Main Menu")
+    println("1.         Create New Playthrough")
+    println("2.         Update a Playthough")
+    println("3.         List All Playthroughs")
+    println("0.         Exit")
+    println()
+    println("Please enter a number: ")
+    input = readLine()!!
+    option = if (input.toIntOrNull() != null && !input.isEmpty())
+        input.toInt()
+    else
+        -9
+    return option
 }
