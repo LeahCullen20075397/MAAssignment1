a54aa6a853eb12b464cc7f95c955177037de1e66
Leah Cullen <leahclcv@gmail.com>
2020-11-02 14:13:15 +0000

    setup logger

--- src/main/kotlin/ie/wit/LeahCullen/A1/main.kt
+++ src/main/kotlin/ie/wit/LeahCullen/A1/main.kt
@@ -1,5 +1,10 @@
 package ie.wit.LeahCullen.A1
 
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger{}
+
 fun main(args: Array<String>){
+    logger.info{"Launching Dragon Age Console App"}
     println("Dragon Age Console App")
 }
