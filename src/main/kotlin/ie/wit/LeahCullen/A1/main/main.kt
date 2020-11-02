e42325070ae6d47dfd9f67028cb02458f20eef82
Leah Cullen <leahclcv@gmail.com>
2020-11-02 16:19:27 +0000

    playthrough creation setup

--- src/main/kotlin/ie/wit/LeahCullen/A1/main/main.kt
+++ src/main/kotlin/ie/wit/LeahCullen/A1/main/main.kt
@@ -80,15 +80,97 @@ fun createMenu(): Int {
 }
 
 fun addCharacter(){
-    println("Adding Character...")
+    var name: String
+    var gender: String
+    var race: String
+    var background: String
+
+    println("Add Your Character")
+    println()
+    println("Enter character name: ")
+    name = readLine()!!
+    println("Enter character gender: ")
+    gender = readLine()!!
+    println("Enter character race: ")
+    race = readLine()!!
+    println("Enter character background: ")
+    background = readLine()!!
+    println("Your character's name is $name . They are $gender and from the $race race. Their background is: $background")
 }
 
 fun editRelationships(){
-    println("Editing Relationships...")
+    var rAlaistair: String
+    var rLeliana: String
+    var rMorrigan: String
+    var rOghren: String
+    var rShale: String
+    var rSten: String
+    var rWynne: String
+    var rZevran: String
+
+    println("Whats your character's relationship to these companions?")
+    println()
+    println("Alistair: ")
+    rAlaistair = readLine()!!
+    println("Leliana: ")
+    rLeliana = readLine()!!
+    println("Morrigan: ")
+    rMorrigan = readLine()!!
+    println("Oghren: ")
+    rOghren = readLine()!!
+    println("Shale: ")
+    rShale = readLine()!!
+    println("Sten: ")
+    rSten = readLine()!!
+    println("Wynne: ")
+    rWynne = readLine()!!
+    println("Zevran: ")
+    rZevran = readLine()!!
+    println("Here are your current relationships: ")
+    println("Alistair: $rAlaistair\nLeliana: $rLeliana\nMorrigan: $rMorrigan" +
+            "\nOghren: $rOghren\nShale: $rShale\nSten: $rSten" +
+            "\nWynne: $rWynne\nZevran: $rZevran")
 }
 
 fun editChoices(){
-    println("Editing Choices...")
+    var hero: String
+    var beast: String
+    var paragon1: String
+    var paragon2: String
+    var mages: String
+    var ruler: String
+    var urn: String
+    var arl: String
+    val archdemon: String
+
+    println("What choices will you make?")
+    println()
+    println("THE BLIGHT")
+    println("Did you die to end the Blight?(yes/no) ")
+    hero = readLine()!!
+    println("THE NATURE OF THE BEAST")
+    println("How did you resolve the problems between the werewolves and the elves? ")
+    beast = readLine()!!
+    println("PARAGON OF HER KIND")
+    println("What happened to the Anvil of the Void? ")
+    paragon1 = readLine()!!
+    println("Who rules in Orzammar? ")
+    paragon2 = readLine()!!
+    println("BROKEN CIRCLE")
+    println("Who did you support at the Broken Circle? ")
+    mages = readLine()!!
+    println("THE LANDSMEET")
+    println("Who now rules Ferelden? ")
+    ruler = readLine()!!
+    println("THE URN OF SACRED ASHES")
+    println("Did you poison the Urn? ")
+    urn = readLine()!!
+    println("THE ARL OF REDCLIFFE")
+    println("What happened to Connor? ")
+    arl = readLine()!!
+    println("THE BATTLE OF DENERIM")
+    println("Who killed the Archdemon? ")
+    archdemon = readLine()!!
 }
 
 fun updatePlaythrough(){
